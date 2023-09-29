0966c73fbebc20a607107e06d55f8861cf341733
Hingaz <hhinga04@gmail.com>
2023-09-29 22:21:27 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", function (event){
+    event.preventDefault();
+
+  let = taskDescription = document.getElementById("new-task-description");
+
+
+  })
+  
   // your code here
 });
+
+
+
